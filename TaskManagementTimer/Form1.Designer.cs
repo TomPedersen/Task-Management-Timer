63cf27fcfdcc308f3ce868ca171c8d30a48afec3
TomPedersen <Knives89@gmail.com>
2015-11-02 16:50:50 +1300

    created new form with a button that starts a timer which results in a popup message every X minutes. Currently set to 1 minute intervals during the development process.

--- TaskManagementTimer/Form1.Designer.cs
+++ TaskManagementTimer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TaskManagementTimer
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startTimer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startTimer
+            // 
+            this.startTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startTimer.Location = new System.Drawing.Point(12, 12);
+            this.startTimer.Name = "startTimer";
+            this.startTimer.Size = new System.Drawing.Size(131, 36);
+            this.startTimer.TabIndex = 0;
+            this.startTimer.Text = "Start Your Timer";
+            this.startTimer.UseVisualStyleBackColor = true;
+            this.startTimer.Click += new System.EventHandler(this.begin_timer);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1183, 668);
+            this.Controls.Add(this.startTimer);
+            this.Name = "MainForm";
+            this.Text = "Task Management Timer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startTimer;
     }
 }
 
--- TaskManagementTimer/Form1.cs
+++ TaskManagementTimer/Form1.cs
@@ -5,16 +5,32 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TaskManagementTimer
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void begin_timer(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                Thread popUp = new Thread((new ThreadStart(ShowMessage)));
+                Thread.Sleep(60 * 1000); //1 minute
+                popUp.Start();
+            }
+        }
+
+        void ShowMessage()
+        {
+            MessageBox.Show("Hi");
         }
     }
 }
