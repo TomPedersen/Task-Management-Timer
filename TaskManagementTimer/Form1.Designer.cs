35942ea1dcb748f30c6e9ee2dda0fd119bbdb24b
TomPedersen <Knives89@gmail.com>
2015-11-03 17:16:37 +1300

    revised design a bit. Added a visual timer countdown that begins with a button and can be paused/stopped with a button. Popup appears when timer reaches 0 and does not automatically reoccur. This will allow user to review/take notes etc. and judge their own break length based on need.

--- TaskManagementTimer/Form1.Designer.cs
+++ TaskManagementTimer/Form1.Designer.cs
@@ -28,7 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.startTimer = new System.Windows.Forms.Button();
+            this.stopTimer = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Minutes = new System.Windows.Forms.Label();
+            this.Seconds = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // startTimer
@@ -42,21 +48,76 @@
             this.startTimer.UseVisualStyleBackColor = true;
             this.startTimer.Click += new System.EventHandler(this.begin_timer);
             // 
+            // stopTimer
+            // 
+            this.stopTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopTimer.Location = new System.Drawing.Point(12, 54);
+            this.stopTimer.Name = "stopTimer";
+            this.stopTimer.Size = new System.Drawing.Size(131, 36);
+            this.stopTimer.TabIndex = 1;
+            this.stopTimer.Text = "Stop Your Timer";
+            this.stopTimer.UseVisualStyleBackColor = true;
+            this.stopTimer.Click += new System.EventHandler(this.stopTimer_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Minutes
+            // 
+            this.Minutes.AutoSize = true;
+            this.Minutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minutes.Location = new System.Drawing.Point(926, 12);
+            this.Minutes.Name = "Minutes";
+            this.Minutes.Size = new System.Drawing.Size(36, 37);
+            this.Minutes.TabIndex = 2;
+            this.Minutes.Text = "0";
+            // 
+            // Seconds
+            // 
+            this.Seconds.AutoSize = true;
+            this.Seconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Seconds.Location = new System.Drawing.Point(1039, 12);
+            this.Seconds.Name = "Seconds";
+            this.Seconds.Size = new System.Drawing.Size(36, 37);
+            this.Seconds.TabIndex = 3;
+            this.Seconds.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(990, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 37);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ":";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1183, 668);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Seconds);
+            this.Controls.Add(this.Minutes);
+            this.Controls.Add(this.stopTimer);
             this.Controls.Add(this.startTimer);
             this.Name = "MainForm";
             this.Text = "Task Management Timer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button startTimer;
+        private System.Windows.Forms.Button stopTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Minutes;
+        private System.Windows.Forms.Label Seconds;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- TaskManagementTimer/Form1.cs
+++ TaskManagementTimer/Form1.cs
@@ -8,11 +8,15 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Timer = System.Timers.Timer;
 
 namespace TaskManagementTimer
 {
     public partial class MainForm : Form
     {
+        public int m = 2;
+        public int s;
+
         public MainForm()
         {
             InitializeComponent();
@@ -20,17 +24,47 @@ namespace TaskManagementTimer
 
         private void begin_timer(object sender, EventArgs e)
         {
-            while (true)
-            {
-                Thread popUp = new Thread((new ThreadStart(ShowMessage)));
-                Thread.Sleep(60 * 1000); //1 minute
-                popUp.Start();
-            }
+            //while (true)
+            //{
+            //    Thread popUp = new Thread((new ThreadStart(ShowMessage)));
+            //    Thread.Sleep(1 * (60 * 1000)); // 1 minute
+            //    popUp.Start();
+            //}
+
+            timer1.Start();
         }
 
         void ShowMessage()
         {
             MessageBox.Show("Hi");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            s = s - 1;
+
+            if (s == -1)
+            {
+                m = m - 1;
+                s = 59;
+            }
+
+            if (m == 0 && s == 0)
+            {
+                timer1.Stop();
+                ShowMessage();
+            }
+
+            string mm = Convert.ToString(m);
+            string ss = Convert.ToString(s);
+
+            Minutes.Text = mm;
+            Seconds.Text = ss;
+        }
+
+        private void stopTimer_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
         }
     }
 }
