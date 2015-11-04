09e182520ef0aee8aa66db7803062c2179883780
TomPedersen <Knives89@gmail.com>
2015-11-04 13:25:05 +1300

    created text boxes and labels for user to keep track of daily tasks and distractions

--- TaskManagementTimer/Form1.Designer.cs
+++ TaskManagementTimer/Form1.Designer.cs
@@ -35,6 +35,27 @@
             this.Minutes = new System.Windows.Forms.Label();
             this.Seconds = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.accomplish1 = new System.Windows.Forms.TextBox();
+            this.accomplish2 = new System.Windows.Forms.TextBox();
+            this.accomplish3 = new System.Windows.Forms.TextBox();
+            this.accomplish4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.justDistract1 = new System.Windows.Forms.TextBox();
+            this.justDistract2 = new System.Windows.Forms.TextBox();
+            this.justDistract3 = new System.Windows.Forms.TextBox();
+            this.justDistract4 = new System.Windows.Forms.TextBox();
+            this.justDistract5 = new System.Windows.Forms.TextBox();
+            this.unjustDistract5 = new System.Windows.Forms.TextBox();
+            this.unjustDistract4 = new System.Windows.Forms.TextBox();
+            this.unjustDistract3 = new System.Windows.Forms.TextBox();
+            this.unjustDistract2 = new System.Windows.Forms.TextBox();
+            this.unjustDistract1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // startTimer
@@ -99,11 +120,212 @@
             this.label3.Text = ":";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // accomplish1
+            // 
+            this.accomplish1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accomplish1.Location = new System.Drawing.Point(34, 155);
+            this.accomplish1.Name = "accomplish1";
+            this.accomplish1.Size = new System.Drawing.Size(300, 26);
+            this.accomplish1.TabIndex = 5;
+            // 
+            // accomplish2
+            // 
+            this.accomplish2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accomplish2.Location = new System.Drawing.Point(34, 187);
+            this.accomplish2.Name = "accomplish2";
+            this.accomplish2.Size = new System.Drawing.Size(300, 26);
+            this.accomplish2.TabIndex = 6;
+            // 
+            // accomplish3
+            // 
+            this.accomplish3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accomplish3.Location = new System.Drawing.Point(34, 219);
+            this.accomplish3.Name = "accomplish3";
+            this.accomplish3.Size = new System.Drawing.Size(300, 26);
+            this.accomplish3.TabIndex = 7;
+            // 
+            // accomplish4
+            // 
+            this.accomplish4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.accomplish4.Location = new System.Drawing.Point(34, 251);
+            this.accomplish4.Name = "accomplish4";
+            this.accomplish4.Size = new System.Drawing.Size(300, 26);
+            this.accomplish4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(34, 283);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(300, 26);
+            this.textBox5.TabIndex = 9;
+            // 
+            // justDistract1
+            // 
+            this.justDistract1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.justDistract1.Location = new System.Drawing.Point(34, 365);
+            this.justDistract1.Name = "justDistract1";
+            this.justDistract1.Size = new System.Drawing.Size(300, 26);
+            this.justDistract1.TabIndex = 10;
+            // 
+            // justDistract2
+            // 
+            this.justDistract2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.justDistract2.Location = new System.Drawing.Point(34, 397);
+            this.justDistract2.Name = "justDistract2";
+            this.justDistract2.Size = new System.Drawing.Size(300, 26);
+            this.justDistract2.TabIndex = 11;
+            // 
+            // justDistract3
+            // 
+            this.justDistract3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.justDistract3.Location = new System.Drawing.Point(34, 429);
+            this.justDistract3.Name = "justDistract3";
+            this.justDistract3.Size = new System.Drawing.Size(300, 26);
+            this.justDistract3.TabIndex = 12;
+            // 
+            // justDistract4
+            // 
+            this.justDistract4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.justDistract4.Location = new System.Drawing.Point(34, 461);
+            this.justDistract4.Name = "justDistract4";
+            this.justDistract4.Size = new System.Drawing.Size(300, 26);
+            this.justDistract4.TabIndex = 13;
+            // 
+            // justDistract5
+            // 
+            this.justDistract5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.justDistract5.Location = new System.Drawing.Point(34, 493);
+            this.justDistract5.Name = "justDistract5";
+            this.justDistract5.Size = new System.Drawing.Size(300, 26);
+            this.justDistract5.TabIndex = 14;
+            // 
+            // unjustDistract5
+            // 
+            this.unjustDistract5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unjustDistract5.Location = new System.Drawing.Point(34, 700);
+            this.unjustDistract5.Name = "unjustDistract5";
+            this.unjustDistract5.Size = new System.Drawing.Size(300, 26);
+            this.unjustDistract5.TabIndex = 19;
+            // 
+            // unjustDistract4
+            // 
+            this.unjustDistract4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unjustDistract4.Location = new System.Drawing.Point(34, 668);
+            this.unjustDistract4.Name = "unjustDistract4";
+            this.unjustDistract4.Size = new System.Drawing.Size(300, 26);
+            this.unjustDistract4.TabIndex = 18;
+            // 
+            // unjustDistract3
+            // 
+            this.unjustDistract3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unjustDistract3.Location = new System.Drawing.Point(34, 636);
+            this.unjustDistract3.Name = "unjustDistract3";
+            this.unjustDistract3.Size = new System.Drawing.Size(300, 26);
+            this.unjustDistract3.TabIndex = 17;
+            // 
+            // unjustDistract2
+            // 
+            this.unjustDistract2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unjustDistract2.Location = new System.Drawing.Point(34, 604);
+            this.unjustDistract2.Name = "unjustDistract2";
+            this.unjustDistract2.Size = new System.Drawing.Size(300, 26);
+            this.unjustDistract2.TabIndex = 16;
+            // 
+            // unjustDistract1
+            // 
+            this.unjustDistract1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.unjustDistract1.Location = new System.Drawing.Point(34, 572);
+            this.unjustDistract1.Name = "unjustDistract1";
+            this.unjustDistract1.Size = new System.Drawing.Size(300, 26);
+            this.unjustDistract1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Tasks For Today";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 323);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 24);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Stuff that \"Just Happened\"";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 530);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 24);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Distractions";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(215, 15);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "What you want to focus on today!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(31, 347);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(306, 15);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Justified distractions, e.g. unplanned meetings";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(31, 554);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(304, 15);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Questionable distractions, e.g. surfing the web";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1183, 668);
+            this.ClientSize = new System.Drawing.Size(1161, 742);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unjustDistract5);
+            this.Controls.Add(this.unjustDistract4);
+            this.Controls.Add(this.unjustDistract3);
+            this.Controls.Add(this.unjustDistract2);
+            this.Controls.Add(this.unjustDistract1);
+            this.Controls.Add(this.justDistract5);
+            this.Controls.Add(this.justDistract4);
+            this.Controls.Add(this.justDistract3);
+            this.Controls.Add(this.justDistract2);
+            this.Controls.Add(this.justDistract1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.accomplish4);
+            this.Controls.Add(this.accomplish3);
+            this.Controls.Add(this.accomplish2);
+            this.Controls.Add(this.accomplish1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Seconds);
             this.Controls.Add(this.Minutes);
@@ -124,6 +346,27 @@
         private System.Windows.Forms.Label Minutes;
         private System.Windows.Forms.Label Seconds;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox accomplish1;
+        private System.Windows.Forms.TextBox accomplish2;
+        private System.Windows.Forms.TextBox accomplish3;
+        private System.Windows.Forms.TextBox accomplish4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox justDistract1;
+        private System.Windows.Forms.TextBox justDistract2;
+        private System.Windows.Forms.TextBox justDistract3;
+        private System.Windows.Forms.TextBox justDistract4;
+        private System.Windows.Forms.TextBox justDistract5;
+        private System.Windows.Forms.TextBox unjustDistract5;
+        private System.Windows.Forms.TextBox unjustDistract4;
+        private System.Windows.Forms.TextBox unjustDistract3;
+        private System.Windows.Forms.TextBox unjustDistract2;
+        private System.Windows.Forms.TextBox unjustDistract1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
