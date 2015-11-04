0e96d9632dbcaf9c3e57ce3c098304cb6f3dbbb6
TomPedersen <Knives89@gmail.com>
2015-11-04 17:26:29 +1300

    extra commentary

--- TaskManagementTimer/Form1.Designer.cs
+++ TaskManagementTimer/Form1.Designer.cs
@@ -677,6 +677,7 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Minutes
